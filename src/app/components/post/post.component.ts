0a66d1645859e7f14d32f80be753672049bb98a3
OmasteWitkowski <owfotografik@gmail.com>
2017-12-05 20:45:02 -0700

    resolve merge

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -13,10 +13,7 @@ export class PostComponent {
         private route: ActivatedRoute,
         private router: Router
     ) {
-<<<<<<< HEAD
         //debugger
-=======
->>>>>>> 11dd73f7a28775edbf12a55db6a66370b5a2fc79
         var id = this.route.snapshot.paramMap.get('id')
         this.post = getPostByPostId(id);
     }
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -8,20 +8,6 @@ class Post {
     id: string;
     title: string;
     body: string;
-<<<<<<< HEAD
-    author: Author
-    constructor(){
-        this.id = posts.length.toString()
-       
-    }
-}
-
-let posts: [Post] = getPostsFromStorage()
-
-function getPostsFromStorage() {
-    return JSON.parse(localStorage.getItem('posts')) || []
-}
-=======
     author: Author;
     constructor() {
         this.id = posts.length.toString()
@@ -34,25 +20,11 @@ function getPostsFromStorage() {
     return JSON.parse(localStorage.getItem('posts')) || []
 }
 
->>>>>>> 11dd73f7a28775edbf12a55db6a66370b5a2fc79
 
 function getPosts() {
     return posts
 }
 
-function updatePost(post) {
-    var p = posts.find(x => x.id == post.id)
-    if(p) {
-        p.body = post.body;
-        p.title = post.title;
-        
-    }
-    else {
-        post.author = {name: "Jake", email: 'j@ja.com', imgUrl: '//placehold.it/80x80' }
-        posts.push(post)
-    }
-    savePosts()
-}
 function savePosts() {
     localStorage.setItem('posts', JSON.stringify(posts));
 }
@@ -65,9 +37,6 @@ function getPostByPostId(postId: string): Post {
             return post
         }
     }
-<<<<<<< HEAD
-    return new Post()
-=======
 
     return new Post()
 }
@@ -82,11 +51,6 @@ function updatePost(post) {
         posts.push(post)
     }
     savePosts()
-}
-
-function savePosts() {
-    localStorage.setItem('posts', JSON.stringify(posts))
->>>>>>> 11dd73f7a28775edbf12a55db6a66370b5a2fc79
 }
 
 
